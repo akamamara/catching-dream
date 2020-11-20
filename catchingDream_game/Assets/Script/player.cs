5f0e99fc224dd52b6c899fc62c4688cf8a110501
akamamara <aldi_okta10@apps.ipb.ac.id>
2020-11-20 18:28:57 +0700

    Update script obstacle & player

---
+++ catchingDream_game/Assets/Script/player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{	
+	public float Xincrement;
+	public float speed;
+	public float maxWidth;
+	public float minWidth;
+	public int health = 5;
+
+	private Vector2 targetPos;
+	private void Update()
+	{
+		// Smooth Speed
+		// transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+		if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < maxWidth)
+		{
+			targetPos = new Vector2(transform.position.x + Xincrement, transform.position.y);
+			transform.position = targetPos;
+		} else if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > minWidth)
+		{
+			targetPos = new Vector2(transform.position.x - Xincrement, transform.position.y);
+			transform.position = targetPos;
+		}
+	}
+}
