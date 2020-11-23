a3da9bb7ec50bb3804942b14ec23cfb3dbaed915
akamamara <aldi_okta10@apps.ipb.ac.id>
2020-11-23 10:34:53 +0700

    Add obstacle pattern logic + fix boundaries asset

---
+++ catchingDream_game/Assets/Script/spawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPoint : MonoBehaviour
+{
+    public GameObject obstacle;
+    
+    // Update is called once per frame
+    void Start()
+    {
+        Instantiate(obstacle, transform.position, Quaternion.identity);
+    }
+}
